5120f4c3ec2e4a9e77187a6f92a3c21f805182b8
mbeckdev <mbeckdev42@gmail.com>
2021-04-01 13:47:11 -0600

    Add object-fit property per image

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const jokes = [
   {
     jokeText: 'APRIL POOLS!',
     jokeImg: 'pools-pexels-ingo-joseph-2618784.jpg',
+    objectFit: 'fill',
   },
   {
     jokeText: 'APRIL MULES!',
     jokeImg: 'mules-pexels-donald-tong-90915.jpg',
+    objectFit: 'contain',
   },
 ];
 
@@ -20,4 +22,6 @@ function hitMeClicked() {
   let randomIndex = Math.floor(Math.random() * jokes.length);
   changingTitle.textContent = jokes[randomIndex].jokeText;
   picture.setAttribute('src', `images/${jokes[randomIndex].jokeImg}`);
+
+  picture.style.objectFit = jokes[randomIndex].objectFit;
 }
