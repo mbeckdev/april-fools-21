b1cbb08445339c301ecd940bf6f6456ab6cc9a54
mbeckdev <mbeckdev42@gmail.com>
2021-04-01 15:00:48 -0600

    Created and styled punchline

--- app.js
+++ app.js
@@ -2,42 +2,42 @@
 
 const jokes = [
   {
-    jokeText: 'APRIL POOLS!',
+    jokeText: 'POOLS!',
     jokeImg: 'pools-pexels-ingo-joseph-2618784.jpg',
     objectFit: 'fill',
   },
   {
-    jokeText: 'APRIL MULES!',
+    jokeText: 'MULES!',
     jokeImg: 'mules-pexels-donald-tong-90915.jpg',
     objectFit: 'contain',
   },
   {
-    jokeText: 'APRIL DROOLS!',
+    jokeText: 'DROOLS!',
     jokeImg: 'drool-alien-41612_640.png',
     objectFit: 'contain',
   },
   {
-    jokeText: 'APRIL FOALS!',
+    jokeText: 'FOALS!',
     jokeImg: 'foal-pexels-elina-sazonova-1838569.jpg',
     objectFit: 'contain',
   },
   {
-    jokeText: 'APRIL JEWELS!',
+    jokeText: 'JEWELS!',
     jokeImg: 'jewel-pexels-engin-akyurt-1458867.jpg',
     objectFit: 'scale-down',
   },
   {
-    jokeText: 'APRIL SCHOOLS!',
+    jokeText: 'SCHOOLS!',
     jokeImg: 'school-pexels-pixabay-207692.jpg',
     objectFit: 'fill',
   },
   {
-    jokeText: 'APRIL STOOLS!',
+    jokeText: 'STOOLS!',
     jokeImg: 'stool-pexels-athena-2180883.jpg',
     objectFit: 'fill',
   },
   {
-    jokeText: 'APRIL TOOLS!',
+    jokeText: 'TOOLS!',
     jokeImg: 'tools-pexels-pixabay-162553.jpg',
     objectFit: 'cover',
   },
@@ -46,11 +46,15 @@ const jokes = [
 document.getElementById('hit-me-btn').addEventListener('click', hitMeClicked);
 
 const changingTitle = document.getElementById('title');
+const punchline = document.getElementById('punchline');
+
 const picture = document.getElementById('pic');
 
 function hitMeClicked() {
   let randomIndex = Math.floor(Math.random() * jokes.length);
-  changingTitle.textContent = jokes[randomIndex].jokeText;
+  // changingTitle.textContent = jokes[randomIndex].jokeText;
+  punchline.textContent = jokes[randomIndex].jokeText;
+
   picture.setAttribute('src', `images/${jokes[randomIndex].jokeImg}`);
 
   picture.style.objectFit = jokes[randomIndex].objectFit;
